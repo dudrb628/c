212ce90f4024f1e3f7ab134970dc71e50689cbdf
ygk <ygk@sowaso.kr>
2025-10-30 11:00:02 +0900

    주석추가

--- 02-03_예제 실습/Triangle/Triangle.cpp
+++ 02-03_예제 실습/Triangle/Triangle.cpp
@@ -1,3 +1,7 @@
+/*	
+* 별표를 사용하여 삼각형을 출력하는 예제
+*/
+
 #include <stdio.h>
 
 void main()
@@ -6,8 +10,8 @@ void main()
 
 	for (i = 1; i <= 15; i = i + 1) {
 		for (j = 0; j < i; j = j + 1) {
-			printf("*");
+			printf("*");			// 별표 하나를 출력한다.
 		}
-		printf("\n");
+		printf("\n");				// 한 줄이 끝나면 개행한다.
 	}
 }

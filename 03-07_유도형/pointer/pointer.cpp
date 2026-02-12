c864e700599b5d914cb9410ba58dae3dc76022df
ygk <ygk@sowaso.kr>
2026-02-12 13:31:35 +0900

    chap 5.3.1 삼항 조건 연산자

--- 03-07_유도형/pointer/pointer.cpp
+++ 03-07_유도형/pointer/pointer.cpp
@@ -13,13 +13,19 @@ void main() {
 
 	pi = &Num;
 	printf("Num의 값은 %d입니다.\n", *pi);
-	printf("Num의 번지수는 %d입니다.\n", &Num);
-	printf("Num의 번지수는 %d입니다.\n", &pi);
 
+	/*
+	printf("Num의 번지수는 %d입니다.\n", &Num);
+	printf("pi의 값은 %d입니다.\n", pi);
+	printf("pi의 번지수는 %d입니다.\n", &pi);
 	printf("Num은 %d입니다.\n", Num);
 	*pi = 628;
 	printf("*pi 값에 628 할당 후 Num은 %d입니다.\n", Num);
+	*/
 
 
-	//pi = 60; 포인터는 할당안되네. *붙여야되네
+	/*
+		pi = 60; 포인터는 할당안되네. *붙여야되네
+		pi에는 *를 붙이고 Num에는 &를 붙이네
+	*/
 }
